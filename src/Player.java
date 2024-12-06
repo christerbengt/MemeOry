ba31b59cece07123bf7bd56d65d7a912dabf5ee9
Christer Bengtsson <mail@christerbengtsson.se>
2024-12-06 11:27:01 +0100

    First version of Player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,25 @@
 public class Player {
+    private int score;
+    private String name;
+
+    // Constructor
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;  // Initialize score to 0
+    }
+
+    // Increment score when player makes a match
+    public void incrementScore() {
+        score++;
+    }
+
+    // Get current score
+    public int getScore() {
+        return score;
+    }
+
+    // Get player name
+    // public String getName() {
+    //    return name;
+    // }
 }
