f951997b0cf7cf43c48308452245d784e79a644f
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2024-12-06 23:00:43 +0100

    Updated design of Buttons, quick fix of bug in displayChooseDifficulty

--- src/Board.java
+++ src/Board.java
@@ -13,9 +13,10 @@ public class Board extends JFrame {
     private final JPanel chooseDifficultyPanel = new JPanel();
     private final JPanel chooseThemePanel = new JPanel();
 
+    private final JLabel startLabel = new JLabel("MemeOry");
     private final JButton startButton = new JButton("Start new game");
-    private final JButton viewHighScore = new JButton("View high scores");
-    private final JButton aboutRules = new JButton("About/Rules");
+    private final JButton viewHighScoreButton = new JButton("View high scores");
+    private final JButton aboutRulesButton = new JButton("About/Rules");
 
     private final JButton levelEasy = new JButton("Easy");
     private final JButton levelHard = new JButton("Hard");
@@ -52,19 +53,24 @@ public class Board extends JFrame {
 
     public void displayStartPanel() {
         setTitle("MemeOry");
-        setLayout(new BorderLayout()); //Kommer behöva hjälp att räkna på komponenters plats sen.... Jennifer
+        setLayout(null); //Kommer behöva hjälp att räkna på komponenters plats sen.... Jennifer
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 700);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
-        getContentPane().setBackground(new Color(255, 222, 222)); //Tyckte grisrosa var passande
+        getContentPane().setBackground(new Color(255, 222, 222)); //Tyckte grisrosa var passande.
 
-        //Buttons
-        add(startPanel, BorderLayout.SOUTH);
-        startPanel.setLayout(new GridLayout(3, 1));
-        startPanel.add(startButton); startPanel.add(viewHighScore);
-        startPanel.add(aboutRules);
+        startPanel.setBounds(0, 0, 700, 700);
+        startPanel.setLayout(null);
+        startPanel.setBackground(new Color(255, 222, 222));
+        add(startPanel);
+        startLabel.setBounds(315, 0, 400, 350);
+        startPanel.add(startLabel);
+        startButton.setBounds(250, 200, 200, 100); viewHighScoreButton.setBounds(250, 300, 200, 100);
+        aboutRulesButton.setBounds(250, 400, 200, 100);
+        startPanel.add(startButton); startPanel.add(viewHighScoreButton);
+        startPanel.add(aboutRulesButton);
 
         startButton.addActionListener(l -> {
             remove(startPanel);
@@ -75,13 +81,28 @@ public class Board extends JFrame {
     }
 
     public void displayChooseDifficulty() {
-        add(chooseDifficultyPanel, BorderLayout.SOUTH);
+        chooseDifficultyPanel.setBounds(0, 0, 700, 700);
+        chooseDifficultyPanel.setLayout(null);
+        chooseDifficultyPanel.setBackground(new Color(255, 222, 222));
+        add(chooseDifficultyPanel);
+        levelEasy.setBounds(250, 200, 200, 100);
+        levelHard.setBounds(250, 350, 200, 100);
         chooseDifficultyPanel.add(levelEasy); chooseDifficultyPanel.add(levelHard);
 
-        levelEasy.addActionListener(l -> chooseDifficulty());
-        levelHard.addActionListener(l -> chooseDifficulty());
-    }
+        levelEasy.addActionListener(l -> {difficulty = DifficultyLevel.EASY;
+        remove(chooseDifficultyPanel);
+        displayChooseTheme();
+        revalidate();
+        repaint();});
 
+        levelHard.addActionListener(l -> {difficulty = DifficultyLevel.HARD;
+            remove(chooseDifficultyPanel);
+            displayChooseTheme();
+            revalidate();
+            repaint();});
+    }
+/* isSelected() fungerar tydligen med JRadioButtons, kan vara värt att ändra knapparna till det i de
+    här fallen. Alltså svårighetsgrad och tema. Bara att ändra om nästa tycker det!
     public void chooseDifficulty() {
         if(levelEasy.isSelected()) {
             difficulty = DifficultyLevel.EASY;
@@ -91,11 +112,17 @@ public class Board extends JFrame {
         remove(chooseDifficultyPanel);
         revalidate();
         repaint();
-        displaChooseTheme();
+        displayChooseTheme();
     }
+ */
 
-    public void displaChooseTheme() {
-        add(chooseThemePanel, BorderLayout.SOUTH);
+    public void displayChooseTheme() {
+        chooseThemePanel.setBounds(0, 0, 700, 700);
+        chooseThemePanel.setLayout(null);
+        chooseThemePanel.setBackground(new Color(255, 222, 222));
+        add(chooseThemePanel);
+        themeAnimals.setBounds(250, 200, 200, 100);
+        themeCharacters.setBounds(250, 350, 200, 100);
         chooseThemePanel.add(themeAnimals); chooseThemePanel.add(themeCharacters);
 
         themeAnimals.addActionListener(l -> chooseTheme());
@@ -116,6 +143,7 @@ public class Board extends JFrame {
     }
 
     public void setBoard(DifficultyLevel difficulty, CardTheme theme) {
+
     }
 
     public static void main(String[] args) {
